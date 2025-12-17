ba62c84e9384af179c87cab2188040a42bda253c
AdamRse <adamrse@gmail.com>
2025-12-17 16:29:10 +0100

    ajout système .env, non testé

--- config/db.php
+++ config/db.php
@@ -1,7 +1,13 @@
 <?php
 
 try {
-    $connexion = new PDO("mysql:host=5.39.77.77;dbname=tp-combat-poo", 'tp_combat', 'V3IIC[s/YCZauem!', [
+    $db_addr=env('DB_ADDR');
+    $db_name=env('DB_NAME');
+    $db_user=env('DB_USER');
+    $db_pwd=env('DB_PASSWORD');
+    $db_port=env('DB_PORT');
+
+    $connexion = new PDO('mysql:host='.$db_addr.':'.$db_port."dbname=$db_name", $db_user, $db_pwd, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 } catch (\Throwable $th) {
