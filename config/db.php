a17256e801408f6f6e364986361d85b4c90fdf63
AdamRse <adamrse@gmail.com>
2025-12-18 12:36:33 +0100

    ajout port connexion string PDO

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ try {
     $db_pwd=env('DB_PASSWORD');
     $db_port=env('DB_PORT');
 
-    $connexion = new PDO('mysql:host='.$db_addr.";dbname=$db_name", $db_user, $db_pwd, [
+    $connexion = new PDO('mysql:host='.$db_addr.":$db_port;dbname=$db_name", $db_user, $db_pwd, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
